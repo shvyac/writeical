6e44a2bd4882d56dd20b511c0b3ba94b52bb6059
shv yac <shvyac@live.jp>
2018-05-18 16:49:05 +0900

    1st

---
+++ ConsoleAppWriteical/ConsoleAppWriteical/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleAppWriteical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines;
+            lines = new string[] {
+            "2018/07/01,http://a.com/sample.pdf,Name"
+            };
+
+            System.Console.WriteLine("BEGIN:VCALENDAR");
+
+            foreach (var line in lines)
+            {
+                var values = line.Split(',');
+                foreach (var value in values)
+                {
+                    //System.Console.WriteLine("{0} ", value);
+                }
+
+                System.Console.WriteLine("BEGIN:VEVENT");
+
+                DateTime DateStart = DateTime.Parse(values[0]);
+                DateTime DateEnd = DateStart.AddDays(1);
+
+                System.Console.WriteLine("DTSTART;VALUE=DATE:" + DateStart.ToString("yyyyMMdd"));
+                System.Console.WriteLine("DTEND;VALUE=DATE:" + DateEnd.ToString("yyyyMMdd"));
+          
+
+                System.Console.WriteLine("SUMMARY:" + values[2] + "");
+                System.Console.WriteLine("LOCATION:" + values[2] + "");
+                System.Console.WriteLine("DESCRIPTION:<a href=\"" + values[1] + "\">" + values[1] + "</a><br>" + 
+                    "<a href=\"" + values[1] + "\">" + values[1] + "</a>" );
+
+                System.Console.WriteLine("END:VEVENT");
+            }
+            System.Console.WriteLine("END:VCALENDAR");
+        }
+    }
+}
